f72bce78a24fb9bd71e00e606c501c53d3166ec2
ShahuSardar-py <shahu.sardar4@gmail.com>
2025-01-07 19:37:24 +0530

    initial commit

---
+++ sum.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a,b;
+    cout<<"enter a";
+    cin>>a;
+
+    cout << "enter b";
+    cin>>b;
+    int sum= a+b;
+    cout<< "Sum is "<< sum;
+
+    return 0;
+
+}
---
+++ test.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int age=18;
+    cout << age <<endl;
+    return 0;
+}
