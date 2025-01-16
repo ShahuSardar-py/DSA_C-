99a889dc7b5c45dab231f4030e8cc613a6496c38
ShahuSardar-py <shahu.sardar4@gmail.com>
2025-01-16 17:36:48 +0530

    pyramid pattern

--- p8.cpp
+++ p8.cpp
@@ -2,14 +2,17 @@
 using namespace std;
 
 int main(){
-    int n=5;
+    int n=6;
+    int num=1;
     for(int i=0;i<n;i++){
-        for(int j=i+1;j>0;j--){
-            cout<<j<< " ";
+        for(int j=0;j=i+1;j++){
+            cout<<num;
+            num++;
+            
 
         }
         cout<<endl;
+
     }
     return 0;
-
 }
