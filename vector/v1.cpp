661bf65e1e748ca8895e6324ef3c6073bc64a611
ShahuSardar-py <shahu.sardar4@gmail.com>
2025-01-21 18:20:49 +0530

    vectors:

---
+++ vector/v1.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main(){
+    vector<int> vec1; //size is 0
+    vector<int>vec2={1,2,3,4,5};
+    cout<<vec2[2]<<endl;
+    return 0;
+}
---
+++ vector/v2.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main(){
+    vector<int>vec={1,2,3,4,5,56,7};
+    for(int i:vec){
+        cout<<i<<endl;
+    
+    }
+return 0;
+}
